1993f5b8c54e71e8ce62649c9748ff8f1b4e963f
MD. Shimul <mdshemul480@gmail.com>
2024-12-28 14:15:53 +0600

    update: macropad code updated

--- ArduinoScripts/arduinoKeypad/arduinoKeypad.ino
+++ ArduinoScripts/arduinoKeypad/arduinoKeypad.ino
@@ -2,45 +2,49 @@
 #define LAYOUT_US_ENGLISH
 #include <HID-Project.h>
 #include <HID-Settings.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 
-
-unsigned long lastTimeButtonStateChanged[7];
+unsigned long lastTimeButtonStateChanged[16];
 unsigned long debounceDuration = 300;
-
-
-
-#define volumePin A0
-uint8_t prevVolume = 0;
 unsigned long lastVolumeUpdateTime = millis();
 
-#define XPin A1
-#define YPin A2
-unsigned long lastGUiUpdateTime = millis();
+const int BUTTON_MAPPED[3][3] = {
+  { 10, 9, 8 },
+  { 7, 6, 5 },
+  { 4, 16, 14 }
+};
+#define BUZZER 15
 
-
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   Serial.begin(9600);
   NKROKeyboard.begin();
   Consumer.begin();
 
-  pinMode(volumePin, INPUT);
-  pinMode(XPin, INPUT);
-  pinMode(YPin, INPUT);
 
-  for (byte pinNum = 2; pinNum <= 6; pinNum++) {
-    pinMode(pinNum, INPUT);
-    lastTimeButtonStateChanged[pinNum] = millis();
+  lcd.begin();
+  lcd.backlight();
+  lcd.print("      NEMO");
+  lcd.setCursor(0, 1);
+  lcd.print("    Keyboard");
+
+  pinMode(BUZZER, OUTPUT);
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      lastTimeButtonStateChanged[BUTTON_MAPPED[i][j]] = millis();
+    }
   }
 }
 
 
-void keyMap(uint8_t mappedButton, uint8_t n, ...) {
-  uint8_t buttonState = digitalRead(mappedButton);
+void keyMap(uint8_t mappedPin, uint8_t n, ...) {
+  uint8_t buttonState = digitalRead(mappedPin);
 
-  if (buttonState && millis() - lastTimeButtonStateChanged[mappedButton] >= debounceDuration) {
-    lastTimeButtonStateChanged[mappedButton] = millis();
+  if (buttonState && millis() - lastTimeButtonStateChanged[mappedPin] >= debounceDuration) {
+    lastTimeButtonStateChanged[mappedPin] = millis();
 
     va_list args;
     va_start(args, n);
@@ -50,7 +54,9 @@ void keyMap(uint8_t mappedButton, uint8_t n, ...) {
       NKROKeyboard.press(KeyboardKeycode(key));
     }
 
+    digitalWrite(BUZZER, HIGH);
     delay(10);
+    digitalWrite(BUZZER, LOW);
 
     va_start(args, n);
     for (uint8_t i = 0; i < n; i++) {
@@ -62,67 +68,21 @@ void keyMap(uint8_t mappedButton, uint8_t n, ...) {
   }
 }
 
-void sliderVolume() {
-  int potValue = analogRead(volumePin);
-  int volume = map(potValue, 0, 1023, 0, 100);
-
-  if (abs(volume - prevVolume) >= 3 && millis() - lastVolumeUpdateTime >= 20) {
-    if (volume > prevVolume) {
-      Consumer.write(MEDIA_VOLUME_UP);
-      Consumer.write(MEDIA_VOLUME_UP);
-    } else {
-      Consumer.write(MEDIA_VOLUME_DOWN);
-      Consumer.write(MEDIA_VOLUME_DOWN);
-    }
-
-    prevVolume = volume;
-    lastVolumeUpdateTime = millis();
-  }
-}
-
 void loop() {
 
-  keyMap(2, 3, KEY_LEFT_GUI, KEY_LEFT_CTRL, KEY_LEFT_ARROW);
-  keyMap(3, 3, KEY_LEFT_GUI, KEY_LEFT_CTRL, KEY_RIGHT_ARROW);
-  keyMap(4, 2, KEY_LEFT_GUI, KEY_TAB);
-  keyMap(5, 1, KEY_RETURN);
-  sliderVolume();
+  keyMap(BUTTON_MAPPED[1][0], 3, KEY_LEFT_GUI, KEY_LEFT_CTRL, KEY_LEFT_ARROW);
+  keyMap(BUTTON_MAPPED[1][2], 3, KEY_LEFT_GUI, KEY_LEFT_CTRL, KEY_RIGHT_ARROW);
+  keyMap(BUTTON_MAPPED[1][1], 2, KEY_LEFT_GUI, KEY_TAB);
+  keyMap(BUTTON_MAPPED[0][1], 1, KEY_RETURN);
 
-  int x = map(analogRead(A1), 0, 1023, 2, 0);
-  int y = map(analogRead(A2), 0, 1023, 2, 0);
+  if (millis() - lastVolumeUpdateTime >= 150) {
+    if (digitalRead(BUTTON_MAPPED[2][2])) {
+      Consumer.write(MEDIA_VOLUME_UP);
+      lastVolumeUpdateTime = millis();
 
-
-  if ((x != 1 || y != 1) && millis() - lastGUiUpdateTime >= 50) {
-    lastGUiUpdateTime = millis();
-    Serial.println(x);
-    Serial.println(y);
-    if (x == 0) {
-      NKROKeyboard.press(KeyboardKeycode(KEY_LEFT_GUI));
-      NKROKeyboard.press(KeyboardKeycode(KEY_LEFT_CTRL));
-      NKROKeyboard.press(KeyboardKeycode(KEY_LEFT_ARROW));
-      delay(10);
-      NKROKeyboard.release(KeyboardKeycode(KEY_LEFT_GUI));
-      NKROKeyboard.release(KeyboardKeycode(KEY_LEFT_CTRL));
-      NKROKeyboard.release(KeyboardKeycode(KEY_LEFT_ARROW));
-
-    } else if (x == 2) {
-      NKROKeyboard.press(KeyboardKeycode(KEY_LEFT_GUI));
-      NKROKeyboard.press(KeyboardKeycode(KEY_LEFT_CTRL));
-      NKROKeyboard.press(KeyboardKeycode(KEY_RIGHT_ARROW));
-      delay(10);
-      NKROKeyboard.release(KeyboardKeycode(KEY_LEFT_GUI));
-      NKROKeyboard.release(KeyboardKeycode(KEY_LEFT_CTRL));
-      NKROKeyboard.release(KeyboardKeycode(KEY_RIGHT_ARROW));
-    }
-
-    if (y == 1) {
-      NKROKeyboard.press(KeyboardKeycode(KEY_LEFT_GUI));
-      NKROKeyboard.press(KeyboardKeycode(KEY_TAB));
-      delay(10);
-      NKROKeyboard.release(KeyboardKeycode(KEY_LEFT_GUI));
-      NKROKeyboard.release(KeyboardKeycode(KEY_TAB));
+    } else if (digitalRead(BUTTON_MAPPED[2][1])) {
+      Consumer.write(MEDIA_VOLUME_DOWN);
+      lastVolumeUpdateTime = millis();
     }
   }
-
-  // delay(200);
 }
